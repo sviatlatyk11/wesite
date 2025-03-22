ad2fd83f92b072f6dd589addb28373bd4c566ae8
Forstrr <na.fisheruk@gmail.com>
2025-03-22 17:44:03 +0000

    Working Includes Accordian - no animation

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const resultContents = document.querySelectorAll(".results__content");
 const resultBtnLeft = document.querySelector(".results__btn--left");
 const resultBtnRight = document.querySelector(".results__btn--right");
 
+const includes = document.querySelectorAll(".include");
+const includesBtnLeft = document.querySelector(".includes__btn--left");
+const includesBtnRight = document.querySelector(".includes__btn--right");
+
 // Results Accordian
 let currentResult = 0;
 
@@ -38,3 +42,67 @@ const prevResult = function (e) {
 
 resultBtnRight.addEventListener("click", nextResult);
 resultBtnLeft.addEventListener("click", prevResult);
+
+// Includes Accordian
+let currentInclude = 1;
+
+const setIncludes = () => {
+  resetIncludes();
+  staticIncludes();
+
+  let visibleIncludes = [prevInclude(), currentInclude, nextInclude()];
+  //   Adding Hidden
+  includes.forEach((el) => {
+    if (!visibleIncludes.includes(Number(el.dataset.tab) - 1))
+      el.classList.add("include--hidden");
+  });
+};
+
+const prevInclude = () => {
+  if (currentInclude - 1 >= 0) return currentInclude - 1;
+  return 4;
+};
+
+const nextInclude = () => {
+  if (currentInclude + 1 <= 4) return currentInclude + 1;
+  return 0;
+};
+
+// Remove all existing helper classes
+const resetIncludes = () => {
+  includes.forEach((el) => {
+    // Remove Hidden
+    el.classList.remove("include--hidden");
+
+    // Remove Column Placement
+    for (let i = 1; i < 4; i++) {
+      el.classList.remove(`include--${i}`);
+    }
+  });
+};
+
+// Adding Column Placement
+const staticIncludes = () => {
+  includes[currentInclude].classList.add("include--2");
+  includes[prevInclude()].classList.add("include--1");
+  includes[nextInclude()].classList.add("include--3");
+};
+
+const nextCurrentInclude = () => {
+  if (currentInclude + 1 < 5) currentInclude++;
+  else currentInclude = 0;
+
+  setIncludes();
+};
+
+const prevCurrentInclude = () => {
+  if (currentInclude - 1 > 0) currentInclude--;
+  else currentInclude = 4;
+
+  setIncludes();
+};
+
+setIncludes();
+
+includesBtnLeft.addEventListener("click", prevCurrentInclude);
+includesBtnRight.addEventListener("click", nextCurrentInclude);
